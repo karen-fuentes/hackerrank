20e7db2241b836fe277c3f1241353f2f4c637272
Karen Fuentes <fuentes.karen92@gmail.com>
2017-12-13 19:11:52 -0500

    2D array solution

--- Arrays/2DArr.playground/Contents.swift
+++ Arrays/2DArr.playground/Contents.swift
@@ -3,3 +3,33 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let arr = Array(repeating:Array(repeating: 0, count: 6), count: 6)
+
+let testArr = [[1,1,1,0,0,0],
+               [0,1,0,0,0,0],
+               [1,1,1,0,0,0],
+               [0,0,2,4,4,0],
+               [0,0,0,2,0,0],
+               [0,0,1,2,4,0]]
+
+func maxSumHourGlass(_ input: [[Int]]) -> Int{
+    var maxSum = -9 * 7
+    for x in 0...3 {
+        for y in 0...3{
+            let top = input[x][y] + input[x][y+1] + input[x][y+2]
+            let middle = input[x][y]
+            let bottom = input[x+2][y] + input[x+2][y+1] + input[x+2][y+2]
+            
+            let sum = top + middle + bottom
+            
+            if sum > maxSum {
+                maxSum = sum
+            }
+        }
+    }
+    return maxSum
+}
+
+
+maxSumHourGlass(testArr)
