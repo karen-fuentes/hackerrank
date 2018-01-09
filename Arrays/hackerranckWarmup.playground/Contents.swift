be0495eaf14e4ce61d926e547847f14836b21983
Karen Fuentes <fuentes.karen92@gmail.com>
2018-01-08 23:14:21 -0500

    converting to military time

--- Arrays/hackerranckWarmup.playground/Contents.swift
+++ Arrays/hackerranckWarmup.playground/Contents.swift
@@ -39,17 +39,77 @@ for i in 0..<numberArr.count {
         
 }
 
-print("""
-    \(positiveNumbCounter/Double(numberArr.count))
-    \(negativeNumbCounter/Double(numberArr.count))
-    \(zeroCounter/Double(numberArr.count))
-""")
+//print("""
+//    \(positiveNumbCounter/Double(numberArr.count))
+//    \(negativeNumbCounter/Double(numberArr.count))
+//    \(zeroCounter/Double(numberArr.count))
+//""")
 
 // printing hashtag staircase
 var hash = "#"
-for i in 0..<4 {
+let n2 = 3
+for i in 0..<n {
 
-    let spaces = (String(repeatElement(" ", count: 4-i-1)))
-    print(spaces + hash)
+    let _ = (String(repeatElement(" ", count: n2-i-1)))
+   // print(spaces + hash)
     hash += "#"
 }
+
+
+// mini max sum
+let numbArr = [1,2,3,4,5]
+var largestElement = Int.min
+var smallestElement = Int.max
+var totalSum = 0
+
+for i in 0..<numbArr.count {
+    totalSum += numbArr[i]
+    if numberArr[i] > largestElement {
+        largestElement = numbArr[i]
+    }
+    if numbArr[i] < smallestElement {
+        smallestElement = numbArr[i]
+    }
+}
+
+//print(totalSum - largestElement, totalSum - smallestElement)
+
+//// candles
+let arrCandle = [3,2,1,3]
+//let numbers = readLine()!.components(separatedBy: " ").map{ Int($0) }
+let maxNumber = arrCandle.max()
+let occurrences = arrCandle.filter { $0 == maxNumber }
+//print(occurrences.count)
+
+
+var input = "12:45:54PM"
+var s = "hello:bob:how"
+//s.split(separator: {$0 == ":"}).map(String.init)
+let q = s.split{$0 == ":"}.map(String.init)
+print(q)
+
+
+let amPmSliceEnd = input.endIndex
+let amPmSliceStart = input.index(amPmSliceEnd, offsetBy: -2)
+    //amPmSliceEnd.advancedBy(-2)
+let amPm = input[amPmSliceStart..<amPmSliceEnd]
+var time = input[input.startIndex..<amPmSliceStart]
+var hrsMinSecArray = time.split{$0 == ":"}.map(String.init)
+print(hrsMinSecArray)
+var hr = hrsMinSecArray[hrsMinSecArray.startIndex]
+var hrInt: Int = Int(hr)!
+
+if amPm == "PM" && hrInt != 12 {
+    hrInt += 12
+    hrsMinSecArray[0] = String(hrInt)
+    time = hrsMinSecArray[0] + ":" + hrsMinSecArray[1] + ":" + hrsMinSecArray[2]
+} else if amPm == "AM" && hrInt == 12 {
+    hrsMinSecArray[0] = "00"
+    time = hrsMinSecArray[0] + ":" + hrsMinSecArray[1] + ":" + hrsMinSecArray[2]
+}
+
+
+print(time)
+//let trimmedInput = input[input.startIndex...input.index(input.endIndex, offsetBy: -3)]
+//print(trimmedInput)
+
